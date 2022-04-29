de4e8785c5a46bef10c8cbddc511177e5dd1c154
Mats01 <matej.butkovic@gmail.com>
2022-04-29 19:44:33 +0200

    select top 20

--- first.py
+++ first.py
@@ -1,4 +1,5 @@
 from unittest import result
+from soupsieve import select
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -18,9 +19,6 @@ database = 'AdventureWorksDW2019'
 SQL_SERVER_CONNECTION_STRING = 'Server=%s;Database=%s;User Id=%s;Password=%s;' % (server, database, user, password)
 
 
-
-
-st.title('Some data')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
@@ -57,6 +55,42 @@ def get_cinjenicne_tablice():
         cursor.close()
         # conn.commit()
         conn.close()
+        data_load_state.text('')
+    except Exception as e:
+        data_load_state.text('there was an error!')
+    
+    print(results)
+
+    # data = pd.DataFrame(results, columns = ['id','func'])
+    data = pd.DataFrame(results)
+    print(data)
+  
+
+def run_query():
+    global option
+    code = "SELECT TOP %d  * FROM %s " % (20, option) 
+    execute_req(code)
+  
+def execute_req(naredba):    
+    global code_block, data
+    re_result = re.search(r"Server=(.+);Database=(.+);User Id=(.+);Password=(.+);", title)
+    server = re_result.group(1)
+    user = re_result.group(3)
+    database = re_result.group(2)
+    password = re_result.group(4)
+    results = []
+    
+    try:
+        conn = pymssql.connect(server, user, password, database)
+        cursor = conn.cursor()
+        cursor.execute(naredba)
+        row = cursor.fetchone()
+        while row:
+            results.append(row)
+            row = cursor.fetchone()
+        cursor.close()
+        # conn.commit()
+        conn.close()
         data_load_state.text('Loading data...done!')
     except Exception as e:
         data_load_state.text('there was an error!')
@@ -66,26 +100,22 @@ def get_cinjenicne_tablice():
     # data = pd.DataFrame(results, columns = ['id','func'])
     data = pd.DataFrame(results)
     print(data)
-    return data
-  
+    
+    code_block = st.code(naredba, language='sql')
+    st.subheader('Recimo tablica')
+    st.write(data)
   
   
 
-
-
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = get_cinjenicne_tablice()
-# Notify the reader that the data was successfully loaded.
-# data_load_state.text('Loading data...done!')
+data_load_state = st.text('')
+get_cinjenicne_tablice()
 
 
 title = st.sidebar.text_input("connection string", value=SQL_SERVER_CONNECTION_STRING, max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=get_cinjenicne_tablice, args=None, kwargs=None,  placeholder=None, disabled=False)   
 
 
 
-option = st.sidebar.selectbox("odaberite cinjenicnu tablicu", options=[opt for opt in cinenjicne_tablice])
+option = st.sidebar.selectbox("odaberite cinjenicnu tablicu", options=[opt.strip() for opt in cinenjicne_tablice])
 
 
 
@@ -104,9 +134,12 @@ with st.sidebar.expander("Dimenzije"):
         except:
             st.write("vise atributa ima isto ime")
                 
-             
-st.subheader('Recimo tablica')
-st.write(data)
+
+
+
+
+st.sidebar.button('Pokreni', on_click=run_query)
+
 
 st.markdown(
     """
